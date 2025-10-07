5e03eedaf79814b1611a36ec9dfe29737b6f8417
Alexander <pchp250g8@tutamail.com>
2025-10-07 15:09:23 +0300

    HelloWorld2

---
+++ HelloWorld2/HelloWorld2.pas
@@ -0,0 +1,7 @@
+﻿Program HelloWorld2;
+begin
+  Console.WriteLine('Hello, World!!!');
+  Console.WriteLine('Input your name:');
+  var Name:=Console.ReadLine();
+  Console.WriteLine('Hello, ' + Name + '!!!');
+end.
